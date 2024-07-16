9c73a019f92d0a8fba4db00eeae066fc2c202a1a
Optics-Boss <->
2024-07-16 21:14:19 +0200

    We now can create files

--- frontend/scripts/library.js
+++ frontend/scripts/library.js
@@ -1,49 +1,9 @@
-this.blogs;
-
-function getBlogs() {
-  this.getJson("http://localhost:3030/blogs")
-  .then((blogs) => {
-    this.blogs = blogs;
-    console.table(this.blogs);
-  }).then(() => {
-    for (let i = 0; i < this.blogs.length; i++) {
-      this.appendElement("blogs_list", 
-        `<li 
-        onclick="goToQuiz('${this.blogs[i].title}')" 
-        class="normal__list-item rounded_corners" 
-        >
-        ${this.blogs[i].title}
-        </li>`
-      );
-    }
-  });
-}
-
-function createBlog() {
-  this.postJson("http://localhost:3030/blogs", { title: "create", content: "this is a test" })
-}
-
-this.getBlogs();
-this.createBlog();
-
 function getJson(from) {
   return fetch(from)
   .then((response) => response.json())
   .then((json) => { 
     return json;
   })
-}
-
-function postJson(url, object) {
- fetch(url, {
-  method: "POST",
-  body: JSON.stringify({
-     title: "create", content: "this is a test" 
-  }),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8"
-  }
-}); 
 }
 
 function setElement(elementId, elementText) {
--- src/blogs.rs
+++ src/blogs.rs
@@ -4,16 +4,23 @@ pub mod blogs {
     use serde::Serialize;
     use serde::Deserialize;
 
+    #[derive(Serialize, Deserialize, Clone, Debug)]
+    pub struct BlogPostCreate {
+        pub title: String,
+        pub content: String,
+    }
+
     #[derive(Serialize, Deserialize, Clone)]
     pub struct BlogPost {
         pub id: i32,
-        pub titel: String,
+        pub title: String,
         pub content: String,
     }
 
     pub fn blogposts() -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
         blogposts_list()
             .or(blogposts_create())
+            .or(frontend())
             .or(index())
     }
 
@@ -21,6 +28,12 @@ pub mod blogs {
         warp::path!()
             .and(warp::get())
             .and(warp::fs::dir("frontend"))
+    }
+
+    pub fn frontend() -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
+        warp::path!("scripts")
+            .and(warp::get())
+            .and(warp::fs::file("frontend/scripts/index.js"))
     }
 
     pub fn blogposts_list() -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
@@ -38,11 +51,13 @@ pub mod blogs {
 }
 
 mod handler {
-    use std::{convert::Infallible, fs::File, vec};
+    use std::{convert::Infallible, fs::File, vec, fs::OpenOptions};
     use csv::StringRecord;
     use warp::http::StatusCode;
 
     use crate::blogs::blogs::BlogPost;
+
+    use super::blogs::BlogPostCreate;
 
     pub async fn list_blogs() -> Result<impl warp::Reply, Infallible> {
 
@@ -52,13 +67,13 @@ mod handler {
 
         for (index, line) in reader.records().into_iter().enumerate() {
             let record = StringRecord::from(line.unwrap());
-            let titel = record.get(0); 
+            let title = record.get(0); 
             let content = record.get(1); 
 
             blogs.push(
                 BlogPost { 
                     id: (index + 1) as i32, 
-                    titel: String::from(titel.unwrap_or("")), 
+                    title: String::from(title.unwrap_or("")), 
                     content: String::from(content.unwrap_or("")) 
                 }
             );
@@ -67,12 +82,16 @@ mod handler {
         Ok(warp::reply::json(&blogs))
     }
 
-    pub async fn create_blog(post: BlogPost) -> Result<impl warp::Reply, Infallible> {
-        let file = File::open("database/test.csv").expect("Can't open file");
+    pub async fn create_blog(post: BlogPostCreate) -> Result<impl warp::Reply, Infallible> {
+        let file = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open("database/test.csv")
+            .unwrap();
+
         let mut writer = csv::Writer::from_writer(file);
 
-        writer.write_record(&[post.titel, post.content]).expect("Can't write record to file");
-        writer.flush().expect("Can't flush file");
+        writer.write_record(&[post.title, post.content]).expect("Can't write record to file");
 
         Ok(StatusCode::CREATED)
     }
